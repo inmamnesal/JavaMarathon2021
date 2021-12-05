d9754bdfc595516a6dbb32784bb742779af628b6
Denis C <denischirchir@gmail.com>
2021-12-05 15:57:11 +0300

    day0 finish

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -2,6 +2,31 @@ package day0;
 
 public class Task1 {
     public static void main(String[] args) {
+        /*int i = 0;
+        while (i < 10) {
+            System.out.print("JAVA ");
+            i++;
+        }*/
+
+        /*for (int i=0; i<8; i++){
+            System.out.print("JAVA ");
+        }*/
+
+        /*int year = 1980;
+        while (year <= 2020){
+            System.out.println("Олимпиада " + year + " года");
+            year += 4;
+        }*/
+
+        /*for (int year = 1980; year <= 2020; year += 4){
+            System.out.println("Олимпиада " + year + " года");
+        }*/
+
+
+        int k = 4;
+        for(int i = 1; i < 10; i ++){
+            System.out.println(i + " * " + k + " = " + i*k);
+        }
 
     }
 }
